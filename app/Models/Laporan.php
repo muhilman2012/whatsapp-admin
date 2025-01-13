202b1bb6efa11964e9a5281b85fcea2372aa5281
muhilman2012 <muhi7714@gmail.com>
2025-01-13 15:02:01 +0700

    Update Unit Category

--- app/Models/Laporan.php
+++ app/Models/Laporan.php
@@ -218,9 +218,9 @@ class Laporan extends Model
         'Asisten Deputi Ekonomi dan Keuangan' => ['Ekonomi dan Keuangan', 'Pemulihan Ekonomi Nasional'],
         'Asisten Deputi Wawasan Kebangsaan, Pertahanan, dan Keamanan' => ['Ketentraman, Ketertiban Umum, dan Perlindungan Masyarakat', 'TNI/Polri', 'Politik dan Hukum', 'Politisasi ASN', 'SP4N Lapor', 'Netralitas ASN', 'Pencegahan dan Pemberantasan Penyalahgunaan dan Peredaran Gelap Narkotika dan Prekursor Narkotika (P4GN)', 'Manajemen ASN', 'Luar Negeri', 'Pertanahan', 'Daerah Perbatasan', 'Pelayanan Publik'], //hapus setelah TNI/Polri
         'Asisten Deputi Infrastruktur, Ketahanan Energi, dan Sumber Daya Alam' => ['Lingkungan Hidup dan Kehutanan', 'Pekerjaan Umum dan Penataan Ruang', 'Pertanian dan Peternakan', 'Energi dan Sumber Daya Alam', 'Mudik', 'Perairan', 'Perhubungan', 'Perumahan'],
-        'Asisten Deputi Politik, Hukum, dan Otonomi Daerah' => ['Politik dan Hukum', 'Politisasi ASN', 'Netralitas ASN', 'Pencegahan dan Pemberantasan Penyalahgunaan dan Peredaran Gelap Narkotika dan Prekursor Narkotika (P4GN)', 'Daerah Perbatasan', 'Pertanahan'],
+        'Asisten Deputi Politik, Hukum, dan Otonomi Daerah' => ['Politik dan Hukum', 'Pencegahan dan Pemberantasan Penyalahgunaan dan Peredaran Gelap Narkotika dan Prekursor Narkotika (P4GN)', 'Daerah Perbatasan', 'Pertanahan'],
         'Asisten Deputi Penanggulangan Kemiskinan' => ['Sosial dan Kesejahteraan'],
-        'Asisten Deputi Tata Kelola Pemerintahan' => ['SP4N Lapor', 'Manajemen ASN', 'Pelayanan Publik'],
+        'Asisten Deputi Tata Kelola Pemerintahan' => ['SP4N Lapor', 'Manajemen ASN', 'Pelayanan Publik', 'Politisasi ASN', 'Netralitas ASN'],
         'Asisten Deputi Industri, Perdagangan, Pariwisata, dan Ekonomi Kreatif' => ['Perlindungan Konsumen', 'Teknologi Informasi dan Komunikasi', 'Pariwisata dan Ekonomi Kreatif', 'Industri dan Perdagangan'],
         'Asisten Deputi Hubungan Luar Negeri' => ['Luar Negeri'],
         'Biro Perencanaan dan Keuangan' => ['Topik Khusus', 'Topik Lainnya', 'Bantuan Masyarakat'],
